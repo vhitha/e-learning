371d11fdbaa79b86a6b6c598da382b702c3de416
ekaphita@gmail.com <ekaphita@gmail.com>
2021-01-05 20:52:19 +0700

    first commit

---
+++ application/views/auth/login.php
@@ -0,0 +1,43 @@
+
+
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+                        <div class="login-logo">
+                            <div class="logo">
+                                <img src="<?=base_url()?>assets/images/user/LOG.png" alt="Cool Admin" />
+                            </div>
+                        </div>
+                        <div class="login-form">
+                            <form action="<?= base_url('user/prosesLogin')?>" method="post">
+                                <div class="form-group">
+                                    <label>Email Address</label>
+                                    <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
+                                </div>
+                                <div class="login-checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember">Remember Me
+                                    </label>
+                                </div>
+                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
+                                
+                            </form>
+                            <div class="register-link">
+                                <p>
+                                    Don't you have account?
+                                    <a href="<?= base_url('user/registerSiswa')?>">Sign Up Here</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
